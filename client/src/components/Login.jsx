72d0c4a1810e2f081f3aaf3db82fabfc1c482c52
Sathwik <sathwik2625@gmail.com>
2024-07-24 02:47:08 +0530

    fixing bug

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,58 +1,58 @@
-import React,{useState} from 'react'
-import axios from 'axios';
+import React, { useState } from 'react'
+import axios from 'axios'
 import request from '../api/request';
 
 const Login = () => {
-    const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [message, setMessage] = useState("");
-  const [success, setSuccess] = useState(null);
+  const [ username,setUserName ] = useState("");
+  const [ password,setPassword ] = useState("");
+  const [ message,setMessage ] = useState("");
+  const [ success,setSuccess ] = useState(null);
   const [ isLoading,setIsLoading ] = useState(false);
 
   const handleLogin = async (e)=>{
     e.preventDefault();
+    setIsLoading(true);
     try {
-        setIsLoading(true)
-        const result = await axios.post(`${request.baseUrl}/api/v1/user/login`,{
-            username:username,
-            password:password
-        })
-        setMessage(result.data.message);setSuccess(result.data.success);setUsername("");setPassword("");
+      const result = await axios.post(`${request.baseUrlDev}/api/v1/user/login`,{
+        username,
+        password
+      })
+      console.log(result);
+      setMessage(result.data.message);setSuccess(result.data.success);setUserName("");setPassword("");
     } catch (error) {
-      setMessage(error.response.data.message);
-      setSuccess(error.response.data.success);
-    }finally{
-        setIsLoading(false)
-      }
-      setTimeout(() => {
-        setMessage("");
-        setSuccess(null);
-      }, 4000);
+      console.log(error);
+      setMessage(error.response.data.message)
+      setSuccess(error.response.data.success)
+    } finally{
+      setIsLoading(false);
+    }
+    setTimeout(() => {
+      setMessage("");setSuccess("");
+    }, 4000);
   }
+
   return (
     <div>
-        <div>{message ? <p>{message}</p> : null}</div>
-        <h1>Login</h1>
-        <form onSubmit={(e)=>handleLogin(e)} >
-            <label>Username</label>
-            <input 
-              type="text" 
-              placeholder="username" 
-              value={username} 
-              required="required"
-              onChange={(e) => setUsername(e.target.value)}
-            ></input>
-            <label>Password</label>
-            <input 
-               type="password" 
-               placeholder="password" 
-               required='required'
-               value={password} 
-               onChange={(e) => setPassword(e.target.value)}
-            ></input>
-            <button type="submit">Login</button>
-        </form>
-        <div> {isLoading ? "Loading....." : null} </div>
+      { message ? <p>{message}</p> : null }
+      <h1>Login</h1>
+      <form onSubmit={handleLogin} >
+      <input 
+        type="text" 
+        placeholder='username'
+        required='required'
+        value={username}
+        onChange= { (e) => setUserName(e.target.value)}
+      />
+      <input 
+        type="text" 
+        placeholder='password'
+        required='required'
+        value={password}
+        onChange= { (e) => setPassword(e.target.value)}
+      />
+      <button type='submit' >Login</button>
+      </form>
+      { isLoading ? <p>Loading...</p> : null }
     </div>
   )
 }
